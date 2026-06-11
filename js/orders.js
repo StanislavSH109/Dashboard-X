d93cce24ae45a7bc9d52b2793055b48ddfc61f1f
Stanislav Shastov <shastov.stas1991@gmail.com>
2026-06-11 11:27:38 +0300

    first init

---
+++ js/orders.js
@@ -0,0 +1,18 @@
+const Orders = [ {
+  productName: 'Javascript Tutorial',
+  productNumber: '85743',
+  paymentStatus: 'Due',
+  status: 'Pending',
+},
+{
+  productName: 'Python Full Course',
+  productNumber: '97245',
+  paymentStatus: 'Refunded',
+  status: 'Declined',
+},
+{
+  productName: 'GRID Tutorial',
+  productNumber: '78784',
+  paymentStatus: 'Paid',
+  status: 'Active',
+}]
